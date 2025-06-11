9bb24a3388e000e4fdee2fd2d15b27832ac45dca
Lucas José Agüero <aguerolucasjose@gmail.com>
2025-06-11 02:45:30 -0300

    Terminando todo

--- planverde/src/include/main.h
+++ planverde/src/include/main.h
@@ -2,52 +2,45 @@
 #define MAIN_H // Definir el header.
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
-enum Cultivo
-{
-    H,
-    F,
-    A,
-    L,
-    O
+enum Cultivo {
+    H, F, A, L, O
 };
 
-struct Huerta
-{
+struct Huerta {
     int id;
     string barrio;
     Cultivo tipoCultivo;
     float superficieCultivada;
     float produccionMensual;
-    bool fertilizante;
+    char fertilizante;
 };
 
-void mostrarMenuPrincipal()
-{
-    cout << "\nBienvenido al proyecto de Huertas Urbanas de Plan Verde!" << endl;
-    cout << "Por favor, ingrese el número de la acción que desea realizar:" << endl;
-    cout << "1. Cargar y procesar datos de una huerta" << endl;
-    cout << "2. Mostrar resultados" << endl;
-    cout << "3. Salir" << endl;
+Huerta listaHuertas[100];
+int cantidadHuertas;
+
+void agregarHuerta(const Huerta& huerta) {
+    if (cantidadHuertas < 100) {
+        listaHuertas[cantidadHuertas++] = huerta;
+    } else {
+        cout << "Error: No se pueden cargar más huertas, se alcanzó el límite máximo." << endl;
+    }
 }
 
-void cargarDatoIndividual(Huerta &huertaCargada, string parametro)
-{
-    if (parametro == "barrio")
-    {
+void cargarDatoIndividual(Huerta &huertaCargada, string parametro) {
+    if (parametro == "barrio") {
         cout << "\nIngrese el barrio: ";
         cin >> huertaCargada.barrio;
     }
-    else if (parametro == "tipoCultivo")
-    {
+    else if (parametro == "tipoCultivo") {
         char tipoCultivo;
         cout << "\nElija el tipo de cultivo principal:\n(H): Hortalizas\n(F): Frutas\n(A): Aromáticas\n(L): Legumbres\n(O): Otros" << endl;
         cin >> tipoCultivo;
 
-        switch (tipoCultivo)
-        {
+        switch (tipoCultivo) {
         case 'H':
             huertaCargada.tipoCultivo = H;
             break;
@@ -65,27 +58,28 @@ void cargarDatoIndividual(Huerta &huertaCargada, string parametro)
             break;
         }
     }
-    else if (parametro == "superficieCultivada")
-    {
+    else if (parametro == "superficieCultivada") {
         cout << "\nIngrese la superficie cultivada (en metros cuadrados): ";
         cin >> huertaCargada.superficieCultivada;
     }
-    else if (parametro == "produccionMensual")
-    {
+    else if (parametro == "produccionMensual") {
         cout << "\nIngrese la producción mensual (en kilogramos): ";
         cin >> huertaCargada.produccionMensual;
     }
-    else if (parametro == "fertilizante")
-    {
+    else if (parametro == "fertilizante") {
         char respuesta;
         cout << "Utiliza fertilizante la huerta:\n(S): Si\n(N): No" << endl;
         cin >> respuesta;
-        huertaCargada.fertilizante = (respuesta == 'S' || respuesta == 's');
+        huertaCargada.fertilizante = respuesta;
     }
 }
 
-Huerta cargarDatos()
-{
+int generarID() {
+    static int ultimoID = 0;
+    return ++ultimoID;
+}
+
+Huerta cargarDatos() {
     Huerta huertaCargada;
 
     huertaCargada.id = generarID();
@@ -96,51 +90,105 @@ Huerta cargarDatos()
     cargarDatoIndividual(huertaCargada, "produccionMensual");
     cargarDatoIndividual(huertaCargada, "fertilizante");
 
+    agregarHuerta(huertaCargada);
     return huertaCargada;
 }
 
-int totalHuertas(listaHuertas)
-{
+int totalHuertas() {
+    return cantidadHuertas;
 }
 
-void mostrarProduccionMensual(listaHuertas)
-{
-    for (int i = 0; i < totalHuertas; i++)
-    {
-        cout << "Huerta " << i << ": " << listaHuertas[i].produccionMensual << endl;
+void mostrarProduccionMensual(Huerta lista[], int cantidad) {
+    for (int i = 0; i < cantidad; i++) {
+        cout << "Huerta " << i << ": " << lista[i].produccionMensual << " kg" << endl;
     }
 }
 
-Cultivo cultivoMasFrecuente(listaHuertas)
-{
+int cultivoMasFrecuente(Huerta lista[], int cantidad) {
+    int contador[5] = {0}; // Contadores para cada tipo de cultivo
+    for (int i = 0; i < cantidad; i++) {
+        switch (lista[i].tipoCultivo) {
+            case H:
+                contador[0]++;
+                break;
+            case F:
+                contador[1]++;
+                break;
+            case A:
+                contador[2]++;
+                break;
+            case L:
+                contador[3]++;
+                break;
+            case O:
+                contador[4]++;
+                break;
+            default:
+                break;
+            }   
+    }
+    int maxIndex = contador[0];
+    for (int i = 1; i < 5; ++i) {
+        if (contador[i] > contador[maxIndex]) {
+            maxIndex = i;
+        }
+    }   
+    return maxIndex;
 }
 
-float porcentajeUsoFertilizantes(listaHuertas)
-{
+float porcentajeUsoFertilizantes(Huerta lista[], int cantidad) {
+    int conFertilizante = 0;
+    for (int i = 0; i < cantidad; i++) {
+        if (lista[i].fertilizante == 'S' || lista[i].fertilizante == 's') {
+            conFertilizante++;
+        }
+    }
+    return conFertilizante;
 }
 
-char cultivoMasFrecuente(listaHuertas)
-{
+string nombreCultivo(int c) {
+    switch(c) {
+        case 0: return "Hortalizas";
+        case 1: return "Frutas";
+        case 2: return "Aromáticas";
+        case 3: return "Legumbres";
+        case 4: return "Otros";
+        default: return "Desconocido";
+    }
 }
 
-float superficiePromedioHuerta(listaHuertas)
-{
+int n = 0;
+float superficiePromedioHuerta(Huerta lista[], int cantidad) {
+    for (int i = 0; i < cantidad; i++) {
+        n += lista[i].superficieCultivada;
+    }
+    float promedio = n/cantidad;
+    return promedio;
 }
 
-void mostrarHuertasPorBarrio(listaHuertas)
-{
+void mostrarHuertasPorBarrio(Huerta lista[], int cantidad) {
+    struct Barrio {
+        string nombre;
+        int huertas;
+    };
+    Barrio barrios[100];
+    for (int i = 0; i < cantidad; i++) {
+        barrios[i].nombre = lista[i].barrio;
+        cout << barrios[i].nombre << ": Huerta " << i << endl;
+    }
 }
 
-void mostrarDatos(listaHuertas) // Se debe ingresar un array con las huertas existentes
-{
-    cout << "Huertas registradas: " << totalHuertas(listaHuertas) << endl;
+void mostrarDatos() { 
+    int cantidad = cantidadHuertas;
+    cout << "Huertas registradas: " << totalHuertas() << endl;
     cout << "Producción mensual por huerta (en kg):" << endl;
-    mostrarProduccionMensual(listaHuertas);
-    cout << "Porcentaje de huertas que utilizan fertilizante: " << porcentajeUsoFertilizante(listaHuertas) << endl;
-    cout << "Tipo de cultivo mas frecuente: " << cultivoMasFrecuente(listaHuertas) << endl;
-    cout << "Superficie promedio por huerta: " << superficiePromedioHuerta(listaHuertas) << endl;
+    mostrarProduccionMensual(listaHuertas, cantidadHuertas);
+    cout << "Porcentaje de huertas que utilizan fertilizante: " << porcentajeUsoFertilizantes(listaHuertas, cantidadHuertas) << "%" << endl;
+    int cultivo = cultivoMasFrecuente(listaHuertas, cantidadHuertas);
+    cout << "Tipo de cultivo mas frecuente: " << nombreCultivo(cultivo) << endl;
+    cout << "Superficie promedio por huerta: " << superficiePromedioHuerta(listaHuertas, cantidadHuertas) << endl;
     cout << "Huertas por barrio: " << endl;
-    mostrarHuertasPorBarrio(listaHuertas);
+    mostrarHuertasPorBarrio(listaHuertas, cantidadHuertas);
 }
 
 #endif
--- planverde/src/main.cpp
+++ planverde/src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include "include/main.h"   // Incluye el main header file.
-#include "include/id.h"      // Incluye el id header file.
 
 using namespace std;
 
@@ -9,20 +8,18 @@ int interaction = 0;
 
 int main() {
     cout << "\nBienvenido al proyecto de Huertas Urbanas de Plan Verde!" << endl;
-    cout << "Por favor, ingrese el número de la acción que desea realizar:" << endl;
-    cout << "1. Cargar y procesar datos de una huerta" << endl;
-    cout << "2. Mostrar resultados" << endl;
-    cout << "3. Salir" << endl;
-
     while (1) {
-        huerta datosHuerta;
+        cout << "Por favor, ingrese el número de la acción que desea realizar:" << endl;
+        cout << "1. Cargar y procesar datos de una huerta" << endl;
+        cout << "2. Mostrar resultados" << endl;
+        cout << "3. Salir" << endl;
         cin >> interaction;
         switch (interaction) {
             case 1:
-                datosHuerta.id = generarID();
-                cout << "Usted ha elegido cargar y procesar datos de una huerta. Se le ha asignado el número '" << datosHuerta.id << "' como identificador de huerta." << endl;
+                cout << "Usted ha elegido cargar y procesar datos de una huerta." << endl;
                 cargarDatos();
                 interaction = 0;
+                cout << "La huerta ha sido cargada exitosamente." << endl;
                 break;
             case 2:
                 cout << "Usted ha elegido mostrar los resultados de la huerta." << endl;
